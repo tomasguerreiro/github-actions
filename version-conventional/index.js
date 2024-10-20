5b17ecc6661aec3245f354706db181be991be4c2
Tomas Guerreiro <falatomas@gmail.com>
2024-10-20 13:59:08 -0300

    adicionando code quality independentes

---
+++ version-conventional/index.js
@@ -0,0 +1,17 @@
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+
+async function run() {
+  try {
+    // Executar os comandos, um por um
+    await exec.exec("npm ci");
+    await exec.exec("npm run build");
+    await exec.exec("npm run lint");
+    await exec.exec("npm run tsc:check");
+    await exec.exec("npm run test");
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
