688560096b8c5135f620462cfbca55720c0f2d8a
Tomas Guerreiro <falatomas@gmail.com>
2024-12-02 16:20:29 -0300

    adicionar mensagens de informação ao processo de versionamento e mensagem de conclusão

--- actions/version-conventional/dist/index.js
+++ actions/version-conventional/dist/index.js
@@ -25694,14 +25694,17 @@ async function run() {
         if (process.env.GITHUB_REF === "refs/heads/develop") {
             core.info("Versioning all packages in alpha mode.");
             await exec.exec("npx lerna version --force-publish --force-git-tag --no-changelog --conventional-commits --conventional-prerelease --preid alpha --yes");
+            core.info("Versioning all packages in alpha mode.");
         }
         else if (process.env.GITHUB_REF === "refs/heads/main") {
             core.info("Versioning all packages.");
             await exec.exec("npx lerna version --force-publish --force-git-tag --conventional-commits --conventional-graduate --yes");
+            core.info("Versioning all packages.");
         }
         else {
             core.info("Skipping versioning.");
         }
+        core.info("Lerna versioning completed.");
     }
     catch (error) {
         if (error instanceof Error) {
