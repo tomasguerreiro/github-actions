9c16395a4cdf413f3443f1300214d76c07781c23
Tomas Guerreiro <falatomas@gmail.com>
2024-12-04 00:27:13 -0300

    adicionar comando para adicionar projeto no Vercel durante o deploy

--- actions/deploy-on-vercel/dist/index.js
+++ actions/deploy-on-vercel/dist/index.js
@@ -25704,6 +25704,7 @@ async function run() {
         // Configura o usuário e o e-mail do Git
         await exec.exec("git config --global user.name 'GitHub Actions'");
         await exec.exec("git config --global user.email 'actions@github.com'");
+        await exec.exec("vercel project add");
         const vercelCommand = `vercel --token ${vercelToken} --yes --cwd ${vercelPath}`;
         const tag = process.env.GITHUB_REF;
         if (tag) {
